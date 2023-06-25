213567c3d2071aedc05afcc0f79dd5705a80a217
l-huisman <huisman.l@yahoo.com>
2023-06-25 11:10:11 +0200

    Added several DTO's

---
+++ dtos/UserResponseDTO.ts
@@ -0,0 +1,10 @@
+export default class UserResponseDTO {
+    public id: string;
+    public email: string;
+    public username: string;
+    constructor(id: string, email: string, username: string) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+    }
+}
